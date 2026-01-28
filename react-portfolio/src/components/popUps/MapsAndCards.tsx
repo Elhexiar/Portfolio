f4f837f864441c2fce1eed8df3d846c2c57f2c00
Elhexiar <118454205+Elhexiar@users.noreply.github.com>
2026-01-28 16:23:22 +0100

    Update MapsAndCards.tsx

--- react-portfolio/src/components/popUps/MapsAndCards.tsx
+++ react-portfolio/src/components/popUps/MapsAndCards.tsx
@@ -40,15 +40,6 @@ function MapsAndCards() {
               <Keyword keyword="Blueprints" />
               <Keyword keyword="Software Architecture" />
             </div>
-            <iframe
-              width="560"
-              height="315"
-              src="https://www.youtube.com/embed/NjxNnqTcHhg?si=FCrUa5dWJ8v1mt1T&amp;controls=0&autoplay=1"
-              title="YouTube video player"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerPolicy="strict-origin-when-cross-origin"
-              allowFullScreen
-            ></iframe>
           </div>
           <p
             style={{
